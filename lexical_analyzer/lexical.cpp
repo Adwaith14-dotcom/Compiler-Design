5de0ae1a5a2dbaae7c218725c6f5d861ce923212
Adwaith Raveendran <adwaithr165@gmail.com>
2026-04-17 14:53:52 +0530

    lexical analyser

--- lexical_analyzer/lexical.cpp
+++ lexical_analyzer/lexical.cpp
@@ -1,21 +1,61 @@
 #include <iostream>
+#include <string>
 #include <cctype>
 using namespace std;
 
+string keywords[] = {"int", "float", "if", "else", "while", "return", "for", "char"};
+bool isKeyword(string word) {
+    for(string k : keywords) {
+        if(word == k)
+            return true;
+    }
+    return false;
+}
+bool isOperator(char c) {
+    return (c=='+' || c=='-' || c=='*' || c=='/' || c=='=' || c=='<' || c=='>');
+}
+bool isSpecialSymbol(char c) {
+    return (c=='(' || c==')' || c=='{' || c=='}' || c==';' || c==',');
+}
 int main() {
     string input;
-    cin >> input;
 
-    cout << "Input: " << input << endl;
-
-    for(char c : input) {
-        if(isalpha(c))
-            cout << c << " -> Identifier" << endl;
-        else if(isdigit(c))
-            cout << c << " -> Number" << endl;
-        else
-            cout << c << " -> Operator" << endl;
+    cout << "Enter a statement:\n";
+    getline(cin, input);
+    string token = "";
+    for(int i = 0; i < input.length(); i++) {
+        char c = input[i];
+        if(isalpha(c)) {
+            token += c;
+            while(i+1 < input.length() && isalnum(input[i+1])) {
+                token += input[++i];
+            }
+            if(isKeyword(token))
+                cout << token << " -> Keyword\n";
+            else
+                cout << token << " -> Identifier\n";
+            token = "";
+        }
+        else if(isdigit(c)) {
+            token += c;
+            while(i+1 < input.length() && isdigit(input[i+1])) {
+                token += input[++i];
+            }
+            cout << token << " -> Number\n";
+            token = "";
+        }
+        else if(isOperator(c)) {
+            if(i+1 < input.length() && input[i+1] == '=') {
+                cout << c << "= -> Relational Operator\n";
+                i++;
+            }
+            else {
+                cout << c << " -> Operator\n";
+            }
+        }
+        else if(isSpecialSymbol(c)) {
+            cout << c << " -> Special Symbol\n";
+        }
     }
-
     return 0;
 }
